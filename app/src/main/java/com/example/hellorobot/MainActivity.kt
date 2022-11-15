813ccdfcba87834853ccf15039623711628e6e11
Anastasia Tarasova <tarasova.nastyaaa@gmail.com>
2022-11-15 12:53:18 +0600

    задания 5,6

--- app/src/main/java/com/example/hellorobot/MainActivity.kt
+++ app/src/main/java/com/example/hellorobot/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hellorobot
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -12,48 +13,36 @@ import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mHelloTextView: TextView
-    private lateinit var editText: EditText
-    private var counter: Int = 0
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        mHelloTextView = findViewById(R.id.textView)
-        editText = findViewById(R.id.editText)
+        val firstText: TextView = findViewById(R.id.text_first)
+        val secondText: TextView = findViewById(R.id.text_second)
+        val thirdText: TextView = findViewById(R.id.text_third)
+        val bottomImage: ImageView = findViewById(R.id.image_bottom)
+        val clickImage: ImageView = findViewById(R.id.imageView10)
 
+        bottomImage.setOnClickListener {
+            val phrases = listOf(
+                "Рыбов хочется",
+                "Вставай, нам нужны рыбы",
+                "Продайте рыбов",
+                "Рыбы рыбы",
+                "Наташ, мы все рыбы уронили!")
 
-        var imageView: ImageView = findViewById(R.id.imageView)
-        val textView: TextView = findViewById(R.id.textView)
-        val rootLayout: ConstraintLayout = findViewById(R.id.root_layout)
-        val button_red: Button = findViewById(R.id.button_red)
-        val button_orange: Button = findViewById(R.id.button_orange)
-        val button_green: Button = findViewById(R.id.button_green)
+            val shuffledList = phrases.shuffled()
 
-        button_red.setOnClickListener{
-            textView.text = resources.getText(R.id.button_red)
-            rootLayout.setBackgroundColor(resources.getColor(R.color.red, null))
+            firstText.text = shuffledList[0]
+            secondText.text = shuffledList[1]
+            thirdText.text = shuffledList[2]
         }
 
-        button_orange.setOnClickListener{
-            textView.text = "ORANGE!"
-            rootLayout.setBackgroundColor(resources.getColor(R.color.orange, null))
+        clickImage.setOnClickListener {
+            val intent = Intent(this@MainActivity, AboutActivity::class.java)
+            startActivity(intent)
         }
 
-        button_green.setOnClickListener{
-            textView.text = "GREEN!"
-            rootLayout.setBackgroundColor(resources.getColor(R.color.green, null))
-        }
-
-        imageView.setOnClickListener {
-            if (editText.text.isEmpty()) {
-                mHelloTextView.text = "Hello Robot!"
-            } else {
-                mHelloTextView.text = "Hello, " + editText.text
-            }
-        }
     }
 }
